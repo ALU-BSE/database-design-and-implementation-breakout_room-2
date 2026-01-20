23a4838f0b53a35bdb0a30d72f9c792f2cd7e4f8
prince-nda <p.ndahiro1@alustudent.com>
2026-01-20 14:43:58 +0200

    Update trip dates and times in data.sql

--- data.sql
+++ data.sql
@@ -12,7 +12,7 @@ INSERT INTO passengers (passenger_id, name, phone) VALUES
 
 INSERT INTO trips (trip_id, rider_id, passenger_id, fare, trip_date) VALUES
 (1, 1, 1, 15.50, NOW()),
-(2, 1, 2, 20.00, NOW()),
-(3, 2, 3, 18.75, NOW()),
+(2, 1, 2, 20.00, '2026-01-20 8:30:00'),
+(3, 2, 3, 18.75, '2026-01-20 9:45:00'),
 (4, 3, 1, 12.25, '2026-01-20 11:20:00'),
 (5, 2, 2, 22.00, '2026-01-20 14:00:00');
